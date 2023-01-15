f01462c5f917e2312ed49ad0789ffb0ff94fef92
TomilayoAfolabi <tomilayoafolabi@gmail.com>
2023-01-14 17:57:24 -0700

    about and contact screen updated

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -60,6 +62,36 @@ const DirectoryNavigator = () => {
         </Stack.Navigator>
     )
 }
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            initialRouteName='About'
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+                options={{title:'About'}}
+            />                     
+        </Stack.Navigator>
+    )
+}
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            initialRouteName='Contact'
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{title:'Contact'}}
+            />                     
+        </Stack.Navigator>
+    )
+}
 
 const Main = () => {
     return (
@@ -80,6 +112,16 @@ const Main = () => {
                 name="Directory"
                 component={DirectoryNavigator}
                 options={{title:"Directory"}}
+            />
+            <Drawer.Screen 
+                name="About"
+                component={AboutNavigator}
+                options={{title:"About"}}
+            />
+            <Drawer.Screen 
+                name="Contact"
+                component={ContactNavigator}
+                options={{title:"Contact"}}
             />
 
            </Drawer.Navigator>
